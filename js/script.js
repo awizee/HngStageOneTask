a6e3f43b76713880fcc04e8c1583c65e5e591ee0
awizee <awizeezabest@gmail.com>
2023-09-09 07:32:41 +0100

    latest

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ let time = document.getElementById("currentUTCTime");
 let date = document.getElementById("currentDayOfTheWeek");
 let d = new Date();
 
-const newDate = (date.innerText = d.toLocaleDateString()); //d.getDate() + '/' + d.getMonth() + '/' + d.getFullYear();
+const newDate = date.innerHTML = `${'<span> Date: </span>'} ${d.toLocaleDateString()}`;//d.getDate() + '/' + d.getMonth() + '/' + d.getFullYear();
 
 setInterval(() => {
   let d = new Date();
-  return (currentTime = time.innerText = d.toLocaleTimeString());
+  return time.innerHTML= `${'<span> Time: </span>'} ${d.toLocaleTimeString()}`;
 }, 1000);
